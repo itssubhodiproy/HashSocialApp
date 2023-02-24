92ca98b93f660750baaf5fc66ee07a2bda625bd0
subh-23 <subhodiproy281@gmail.com>
2023-02-24 08:07:57 +0530

    dependency added

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyles}>Open up App.js to start working on your app!</Text>
+      <Text>This is somthing in which I'm working on!</Text>
+      <Text>React Native is great !!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyles: {
+    color: 'red',
+    fontSize: 14,
+    fontStyle: 'italic',
+    fontVariant: ['small-caps'],
   },
 });
