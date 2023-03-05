41f23caed1d1ae099b7a178b82f172212edfb527
subh-23 <subhodiproy281@gmail.com>
2023-03-05 10:27:06 +0530

    more concise design of feed inspired from instagram

--- src/components/Post.js
+++ src/components/Post.js
@@ -30,52 +30,52 @@ export default function Post() {
             uri: "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
           }}
         />
+        <View style={styles.activityRow}>
+          <View style={styles.firstActivityRow}>
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/2/2267.png",
+                }}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/1380/1380338.png",
+                }}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/25/25419.png",
+                }}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Image
+              source={{
+                uri: "https://cdn-icons-png.flaticon.com/512/6165/6165217.png",
+              }}
+              style={styles.icon}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.engagementCount}>
+          <Text>100 Likes</Text>
+          <Text>9 comments</Text>
+        </View>
         <Text style={styles.caption}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </Text>
-      </View>
-      <View style={styles.engagementCount}>
-        <Text>100 Likes</Text>
-        <Text>9 comments</Text>
-      </View>
-      <View style={styles.activityRow}>
-        <View style={styles.firstActivityRow}>
-          <TouchableOpacity style={styles.button}>
-            <Image
-              source={{
-                uri: "https://cdn-icons-png.flaticon.com/512/2/2267.png",
-              }}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Image
-              source={{
-                uri: "https://cdn-icons-png.flaticon.com/512/1380/1380338.png",
-              }}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Image
-              source={{
-                uri: "https://cdn-icons-png.flaticon.com/512/25/25419.png",
-              }}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.button}>
-          <Image
-            source={{
-              uri: "https://cdn-icons-png.flaticon.com/512/6165/6165217.png",
-            }}
-            style={styles.icon}
-          />
-        </TouchableOpacity>
       </View>
     </View>
   );
@@ -84,20 +84,17 @@ export default function Post() {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    borderRadius: 10,
     alignItems: "center",
     marginBottom: 10,
-    width: "99%",
-    borderWidth: 2,
-    borderColor: "#d2d4d2",
+    width: "100%",
   },
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     width: "100%",
-    backgroundColor: "#d2d4d2",
-    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "#d2d4d2",
   },
   firstHeader: {
     flexDirection: "row",
@@ -112,7 +109,8 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   caption: {
-    margin: 10,
+    marginLeft: 10,
+    marginBottom: 10,
   },
   engagementCount: {
     flexDirection: "row",
@@ -126,21 +124,16 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     width: "100%",
-    backgroundColor: "#d2d4d2",
-    padding: 10,
+    paddingTop: 10,
   },
   content: {
-    borderRadius: 10,
     width: "100%",
-    // margin: 20,
   },
   username: {
     marginLeft: 0,
   },
   postImage: {
-    // width: "100%",
     height: 200,
-    // borderRadius: 10,
   },
   icon: {
     width: 20,
